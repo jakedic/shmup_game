9edac84cbb07aff995aadcca0f14573c0b9eff8f
Dylan <dyalndic02@gmail.com>
2026-01-24 12:32:48 -0500

    hey Jake, like I discussed with you last week I added the new texture for the bullet and played around with textures to get a feel for how the software worked. I also added a slight disipation animation for the bullet so players have a better idea of when their bullet will disapear. I tried adding particle effects but I didn't like how they looked so I removed the node but left the texture I used for the particle effects just incase we want to add particle effects to something else in the future

--- classic_shmup-master/bullets/bullet.gd
+++ classic_shmup-master/bullets/bullet.gd
@@ -6,6 +6,7 @@ func _ready():
 	speed = 250
 	damage = 1
 	max_distance = 125.0  # Very short distance for quick disappearance
+	$Bullet_disipation.play("Bullet_disipation")
 
 func start(pos: Vector2, dir: Vector2 = Vector2.UP):
 	super.start(pos, dir)
