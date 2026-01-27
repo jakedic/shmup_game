3d31733820877404658b60081f9f1c4ad6f49676
Dylan <dyalndic02@gmail.com>
2026-01-27 18:38:36 -0500

    l

--- classic_shmup-master/score_multipier_UI_script.gd
+++ classic_shmup-master/score_multipier_UI_script.gd
@@ -1,11 +1,20 @@
 extends HBoxContainer
 
 var digit_coords = {
-	1: Vector2(7, 5),
+	1: Vector2(0, 0),
+	2: Vector2(8, 0),
+	3: Vector2(16, 0),
+	4: Vector2(24, 0),
+	5: Vector2(32, 0),
+	6: Vector2(0, 8),
+	7: Vector2(8, 8),
+	8: Vector2(16, 8),
+	9: Vector2(24, 8),
+	0: Vector2(32, 8)
 }
 
 func _ready():
-	display_digits(1)
+	display_digits(123)
 
 func display_digits(n):
 	var s = "%08d" % n
