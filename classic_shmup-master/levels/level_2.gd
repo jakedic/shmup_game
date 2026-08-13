82d1fe28e8c3bcc472ff22e960a68bc3d69fcd13
Jake <jdicicco6@gatech.edu>
2026-08-13 12:40:53 -0400

    Added in tutorial explainer

--- classic_shmup-master/levels/level_2.gd
+++ classic_shmup-master/levels/level_2.gd
@@ -1,76 +1,23 @@
-extends Node2D
+# level_2.gd
+extends BaseLevel
 
 var enemy_yellow = preload("res://Enemies/enemy_yellow.tscn")
 var enemy_red = preload("res://Enemies/enemy_red.tscn")
-var score = 0
-var playing = false
-
-@onready var start_button = $CanvasLayer/CenterContainer/Start
-@onready var game_over = $CanvasLayer/CenterContainer/GameOver
 
 func _ready():
-	game_over.hide()
-	start_button.show()
-	var tween = create_tween().set_loops().set_parallel(false).set_trans(Tween.TRANS_SINE)
-	tween.tween_property($EnemyAnchor, "position:x", $EnemyAnchor.position.x + 3, 1.0)
-	tween.tween_property($EnemyAnchor, "position:x", $EnemyAnchor.position.x - 3, 1.0)
-	var tween2 = create_tween().set_loops().set_parallel(false).set_trans(Tween.TRANS_BACK)
-	tween2.tween_property($EnemyAnchor, "position:y", $EnemyAnchor.position.y + 3, 1.5).set_ease(Tween.EASE_IN_OUT)
-	tween2.tween_property($EnemyAnchor, "position:y", $EnemyAnchor.position.y - 3, 1.5).set_ease(Tween.EASE_IN_OUT)	
-	
+	# Set up level-specific data (same enemies/wave count as level 1)
+	enemy_scenes = [enemy_yellow, enemy_red]
+	max_waves = 2  # Same wave count as level 1
+
+	# Call parent _ready after setting up level-specific data
+	super._ready()
+
+# Optional: Override spawn pattern for this specific level
 func spawn_enemies():
-	for x in range(9): 
-		for y in range(4):
-			var e
-			if randf()<.5:
-				e = enemy_yellow.instantiate()
-			else:
-				e = enemy_red.instantiate()
-			var pos = Vector2(x * (16 + 8) + 24, 16 * 3 + y * 40)
-			add_child(e)
-			e.start(pos)
-			e.anchor = $EnemyAnchor
-			e.died.connect(_on_enemy_died)
+	super.spawn_enemies()  # Use parent's implementation
 
-func _on_enemy_died(value):
-	score += value
-	$CanvasLayer/UI.update_score(score)
-	$Camera2D.add_trauma(0.5)
-	
-func _process(_delta):
-	if get_tree().get_nodes_in_group("enemies").size() == 0 and playing:
-		spawn_enemies()
-	
-func _on_player_died():
-#	print("game over")
-	playing = false
-	get_tree().call_group("enemies", "queue_free")
-	game_over.show()
-	await get_tree().create_timer(2).timeout
-	game_over.hide()
+# This is the one difference from level 1: once all of this level's waves
+# are cleared, send the player back to the home/title screen instead of
+# trying to advance to a level 3.
+func change_levels():
 	get_tree().change_scene_to_file("res://levels/title_screen.tscn")
-	start_button.show()
-	
-func new_game():
-	score = 0
-	$CanvasLayer/UI.update_score(score)
-
-	# See BaseLevel.new_game() / stats.gd - power bubble power-ups persist
-	# for a level, so clear them out at the start of a fresh run.
-	Stats.clear_level_modifiers()
-
-	$Player.start()
-	spawn_enemies()
-	playing = true 
-
-func _on_start_pressed():
-	start_button.hide()
-	new_game()
-	#spawn_enemies()
-	
-func _input(event):
-	# Use the action you created in Input Map
-	if event.is_action_pressed("start"):
-		# Only start if we're at the start screen
-		if start_button.visible and not playing:
-			_on_start_pressed()
--- classic_shmup-master/levels/title_screen.gd
+++ classic_shmup-master/levels/title_screen.gd
@@ -6,4 +6,8 @@ extends Control
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://levels/level_1.tscn")
-	
+
+
+func _on_tutorial_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://levels/tutorial_screen.tscn")
+
